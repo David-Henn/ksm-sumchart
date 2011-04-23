e97be8f02f5f1d5d62215a5708f621370dcb3961
docnapalm <mischavogt@gmx.de>
2011-04-23 19:46:56 +0200

    first implementation of a passive QAxis...but not exaclty

--- src/pluginGui/lineFunctions/Qaxis.java
+++ src/pluginGui/lineFunctions/Qaxis.java
@@ -15,25 +15,37 @@ public class Qaxis implements Function{
 
     private int xOffset = 0;
     private int yOffset = 0;
+    private int xMax = 0;
     private int yMax = 0;
     private double slope = 1;
+    private int qY = 0;
+    private int qX = 0;
 
-    public Qaxis(int areaHeight, int areaWidth, int borderLength, double axisSlope)
+    public Qaxis(int areaHeight, int areaWidth, int borderLength, double axisSlope, Point QPoint)
     {
         this.yOffset = borderLength;
         this.yMax = (areaHeight - borderLength);
         this.xOffset = borderLength;
+        this.xMax = (areaWidth - borderLength);
         this.slope = axisSlope;
+        this.qY = QPoint.y;
+        this.qX = QPoint.x;
     }
     
     public Point calcY(int x, int y) {
-        if((x>=xOffset) && (y>=yOffset))
+        if((x>=xOffset) && (y>=yOffset) && (x<=xMax) && (y<=yMax))
         {
-            return new Point((int)x, (int)y);
+            int height = 0;
+            int b = 0;
+            b = ((int) ((yMax - qY) - (slope * qX)));
+            height = (int) (slope * x) + b;
+            height = (yMax - height);
+
+            return new Point((int)x, height);
         }
         else
         {
-            return  new Point(0,0);
+            return  new Point(xOffset, yOffset);
         }
 
     }
